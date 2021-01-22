63a93066471ffecb58b3fa638d4578d3571826d0
sslpaiva <sslpaiva@hotmail.com>
2021-01-22 12:24:36 -0300

    ai_teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
-function Home(){
-    return <div> Home </div>
+import {useState} from 'React';
+
+function Contador(){
+    const [contador,setContador] = useState(1);
+    
+    function adicionaContador()
+    {        
+        setContador(contador+1);
+    }
+
+    return (        
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionaContador}> adicionar </button>
+        </div>
+    )
+}
+function Home(){    
+    return ( <Contador/>) ;    
 }
 
 export default Home
